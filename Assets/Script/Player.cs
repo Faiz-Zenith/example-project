4ff16f8c248eb22fe64626bd7f02cae768843ff5
SilDrag45 <136712759+SilDrag45@users.noreply.github.com>
2023-09-22 16:31:02 +0700

    Player.cs

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -13,8 +13,39 @@ public class Player : MonoBehaviour {
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
 
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-        transform.position += moveDir * moveSpeed * Time.deltaTime;
 
+        float moveDistance = moveSpeed*Time.deltaTime;
+        float playerRadius = .7f;
+        float playerHeight = 2f;
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight,playerRadius, moveDir,moveDistance);
+
+        if (!canMove) {
+            // cannot move toward moveDir
+
+            // Attemp only X movement
+            Vector3 moveDirX = new Vector3(moveDir.x,0,0).normalized;
+            canMove=!Physics.CapsuleCast(transform.position,transform.position+Vector3.up*playerHeight,playerRadius,moveDirX,moveDistance);
+        
+            if (canMove) {
+                // can move only on the X
+                moveDir=moveDirX;
+            }else {
+                //cannot move only on the X
+
+                //Attemp only Z movement
+                Vector3 moveDirZ = new Vector3(0,0,moveDir.z).normalized;
+                canMove=!Physics.CapsuleCast(transform.position,transform.position+Vector3.up*playerHeight,playerRadius,moveDirZ,moveDistance);
+            
+                if(canMove) {
+                    //can move only on the z
+                    moveDir=moveDirZ;
+                }
+
+            }
+        }
+        if (canMove) {
+            transform.position+=moveDir *moveDistance;
+        }
         isWalking=moveDir!=Vector3.zero;
 
         float rotatespeed = 20f;
